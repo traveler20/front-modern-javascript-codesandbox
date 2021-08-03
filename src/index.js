3d684c715a06a5df1d1af6f1a266ce94387b66a0
traveler20 <60683849+traveler20@users.noreply.github.com>
2021-08-03 14:55:17 +0900

    🐛Bugfix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./styles.css";
 
 const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
-  const inputText = document.getElementById("add-text").Value;
+  const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
   createIncompleteList(inputText);
